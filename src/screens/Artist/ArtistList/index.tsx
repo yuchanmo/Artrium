2310ed8f0e7e0dfbb08ddfa55409c53c572bb240
mojjijji@gmail.com <mojjijji@gmail.com>
2021-08-17 01:42:17 +0900

    save

--- src/screens/Artist/ArtistList/index.tsx
+++ src/screens/Artist/ArtistList/index.tsx
@@ -1,14 +1,24 @@
 import React,{useState,useEffect, useLayoutEffect} from 'react';
-import { Text, View,StyleSheet, ScrollView } from 'react-native';
+import { Text, View,StyleSheet, ScrollView, SafeAreaView, FlatList } from 'react-native';
+import { SearchBar,Divider } from 'react-native-elements';
 import ArtistInfo from './ArtistInfo';
+
 
 const styles = StyleSheet.create(
     ({
         container: {
             backgroundColor: "rgba(20,31,40,1)",
-            width: 375,
-            height: 644
+            width: '100%',
+         
           },
+          searchbox:{
+            flex:1
+
+          },
+          artistlistContainer:{
+            flex:8
+          },
+
           tweet: {
             height: 148
           },
@@ -38,19 +48,114 @@ const styles = StyleSheet.create(
     })
 );
 
+const samples:Array<ArtistInformation> = [
+  {
+  KorName:'문형태',
+  EngName:'Mun',
+  PictureUrl:'../../../../samples/images/artists/mun.png',
+  Summary:'짱짱맨',
+  DetailInformation:'이보다 더 짱짱맨',
+  UpdateDate:'2021-08-17',
+  ArtList:[{
+    ImageUrl:'../../../../samples/images/artists/mun.png',
+    Material:'print',
+  EstimatePrice:1000000,
+Size:'150x150',
+Title:'이것은 무엇인가'}]
+},
+{
+  KorName:'문형태',
+  EngName:'Mun',
+  PictureUrl:'../../../../samples/images/artists/mun.png',
+  Summary:'짱짱맨',
+  DetailInformation:'이보다 더 짱짱맨',
+  UpdateDate:'2021-08-17',
+  ArtList:[{
+    ImageUrl:'../../../../samples/images/artists/mun.png',
+    Material:'print',
+  EstimatePrice:1000000,
+Size:'150x150',
+Title:'이것은 무엇인가'}]
+},
+{
+  KorName:'문형태',
+  EngName:'Mun',
+  PictureUrl:'../../../../samples/images/artists/mun.png',
+  Summary:'짱짱맨',
+  DetailInformation:'이보다 더 짱짱맨',
+  UpdateDate:'2021-08-17',
+  ArtList:[{
+    ImageUrl:'../../../../samples/images/artists/mun.png',
+    Material:'print',
+  EstimatePrice:1000000,
+Size:'150x150',
+Title:'이것은 무엇인가'}]
+},
+{
+  KorName:'문형태',
+  EngName:'Mun',
+  PictureUrl:'../../../../samples/images/artists/mun.png',
+  Summary:'짱짱맨',
+  DetailInformation:'이보다 더 짱짱맨',
+  UpdateDate:'2021-08-17',
+  ArtList:[{
+    ImageUrl:'../../../../samples/images/artists/mun.png',
+    Material:'print',
+  EstimatePrice:1000000,
+Size:'150x150',
+Title:'이것은 무엇인가'}]
+},
+{
+  KorName:'문형태',
+  EngName:'Mun',
+  PictureUrl:'../../../../samples/images/artists/mun.png',
+  Summary:'짱짱맨',
+  DetailInformation:'이보다 더 짱짱맨',
+  UpdateDate:'2021-08-17',
+  ArtList:[{
+    ImageUrl:'../../../../samples/images/artists/mun.png',
+    Material:'print',
+  EstimatePrice:1000000,
+Size:'150x150',
+Title:'이것은 무엇인가'}]
+},
+];
+
 const ArtistList = ({}) =>{
+
+    let [serachingName,setSearchingName] = useState<string>('');
     return (
         <>
-         <ScrollView horizontal={false} style={styles.container}>
+        <View style={styles.searchbox}>
+            <SearchBar 
+            placeholder="작가이름" 
+            value={serachingName} 
+            onChangeText={(v)=>{setSearchingName(v)}}
+            ></SearchBar>
+        </View>
+        <View style={styles.artistlistContainer}>
+        <SafeAreaView style={styles.container}>
+          
+          
+          <FlatList
+            data={samples}
+            renderItem={(i)=><><ArtistInfo
+              artist={i}
+              style={styles.tweet}
+           ></ArtistInfo>
+           <Divider style={styles.divider}></Divider></>}
+            keyExtractor={(item,i) => item.KorName + i.toString()}
+          />
+          
+       </SafeAreaView>
+       </View>
+         {/* <ScrollView horizontal={false} style={styles.container}>
+          
          <ArtistInfo
-        Content=""
-        CommentNumber="30"
-        RetweetNumber="2K"
-        LikeNumber="50K"
-        Username=""
-        style={styles.tweet}
+            artist={samples}
+            style={styles.tweet}
          ></ArtistInfo>
-        </ScrollView>
+        </ScrollView> */}
         </>
         
     );
